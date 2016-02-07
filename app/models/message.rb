670de72031df1a8cbd0caf42b34eaf2eca6b881d
mary_june <lily.daisy0619@gmail.com>
2016-02-07 08:09:50 +0000

    create_challenge2

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,7 @@ class Message < ActiveRecord::Base
   # 名前は必須入力かつ20文字以内
   validates :name , length: { maximum: 20 } , presence: true
   #年齢は必須入力かつ1以上が入力できる
-  validates :age , presence: true
+  validates :age , numericality: {greater_than_or_equal_to: 0} , presence: true
   # 内容は必須入力かつ2文字以上30文字以下
   validates :body , length: { minimum: 2, maximum: 30 } , presence: true
 end
